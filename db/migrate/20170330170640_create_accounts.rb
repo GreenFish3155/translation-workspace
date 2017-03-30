c14df6535a4b51e3bd2a5f172a12659bceec7144
Jacob Krevat <jkrevat@uncc.edu>
2017-03-30 17:10:03 +0000

    Generate and run Account migration

---
+++ db/migrate/20170330170640_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :accounts do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :username
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
