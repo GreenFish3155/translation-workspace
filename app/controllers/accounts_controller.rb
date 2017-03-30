8aedd541e3385b8f01389c7e621bfe3f6c0687ff
Jacob Krevat <jkrevat@uncc.edu>
2017-03-30 17:26:39 +0000

    Add new form, create action, show action

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,10 +2,29 @@ class AccountsController < ApplicationController
     def index
     end
     
+    def show
+        @account = Account.find(params[:id])
+    end
+    
     def new
-       # @account=Account.new
+       @account = Account.new
+    end
+    
+    def create
+        @account = Account.new(account_params)
+        
+        if @account.save
+            redirect_to @account
+        else
+            render 'new'
+        end
     end
     
     def login
     end
 end
+
+private
+    def account_params
+       params.require(:account).permit(:first_name, :last_name, :username, :password)  
+    end
