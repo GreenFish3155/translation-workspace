fc902c4d6c0f9b71f7bdffe54de69827d4fce719
Jacob Krevat <jkrevat@uncc.edu>
2017-03-30 17:04:29 +0000

    Add function to buttons and some styling

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,11 @@
+class AccountsController < ApplicationController
+    def index
+    end
+    
+    def new
+       # @account=Account.new
+    end
+    
+    def login
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  
+  resources :accounts do
+    collection do
+     get :login
+   end
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
